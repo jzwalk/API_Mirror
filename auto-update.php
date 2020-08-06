03d0476b24027cc7d95eec9fe2fbb433215640d5
羽中 <zyz59@126.com>
2020-08-06 18:39:08 +0800

    Action自动更新修正php脚本路径bug测试重命名打包

--- auto-update.php
+++ auto-update.php
@@ -111,10 +111,10 @@
 								}
 								$authorNames = html_entity_decode(implode($separator,$authors));
 								if ($authorNames!==trim(strip_tags($infos['author']))) {
-									$plugin = $master.($doc ? $paths['1'] : ($sub ? $paths['1'] : '').'/Plugin.php');
+									$plugin = $master.($doc ? $paths['1'] : ($sub ? $paths['1'].'/' : '').'Plugin.php');
 									$codes = file_get_contents($plugin);
 									file_put_contents($plugin,str_replace($infos['author'],$authorNames,$codes));
-									$renamed = '/ Rename Author ';
+									$renamed = $authorNames.' vs '.trim(strip_tags($infos['author'])).'/ Rename Author ';
 								}
 								$cdn = call_user_func('cdnZip',$name['0'],$infos['author']);
 								$phpZip->open($cdn,ZipArchive::CREATE | ZipArchive::OVERWRITE);
