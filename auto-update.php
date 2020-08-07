dedacfab1b89867d32ee8960547bd7eed9d101be
羽中 <zyz59@126.com>
2020-08-07 18:28:23 +0800

    Action自动更新阶段性测试下载5项

--- auto-update.php
+++ auto-update.php
@@ -131,10 +131,8 @@
 									$plugin = $master.($doc ? $paths['1'] : $path);
 									$codes = file_get_contents($plugin);
 									file_put_contents($plugin,str_replace($infos['author'],$authorTitles,$codes));
-									$renamed = $authorTitles.' vs '.trim(strip_tags($infos['author'])).'/ Rename Author ';
+									$renamed = '/ Rename Author ';
 								}
-$s = print_r($line.' & '.$renamed.' & '.($authorTitles==trim(strip_tags($infos['author']))),true);
-file_put_contents('log.txt',$s);
 								$cdn = call_user_func('cdnZip',$name['0'],$infos['author']);
 								$phpZip->open($cdn,ZipArchive::CREATE | ZipArchive::OVERWRITE);
 								if (!$doc) {
